f3e977ef7e3a0c297b5403913aca9fafebbf5128
Ahmed Ismail <iamahmedismail19@gmail.com>
2024-09-02 22:00:50 +0500

    Add some basic testsuite for live-pages

--- test/plots_creator_web/live/shared_with_you_live_test.exs
+++ test/plots_creator_web/live/shared_with_you_live_test.exs
@@ -3,6 +3,8 @@ defmodule PlotsCreatorWeb.SharedWithYouLiveTest do
 
   import PlotsCreator.DashboardFixtures
 
+  setup :register_and_log_in_user
+
   defp create_shared_with_you(_) do
     shared_with_you = shared_with_you_fixture()
     %{shared_with_you: shared_with_you}
@@ -10,9 +12,19 @@ defmodule PlotsCreatorWeb.SharedWithYouLiveTest do
 
   describe "Index" do
     setup [:create_shared_with_you]
+
+    test "renders the index page", %{conn: conn} do
+      conn = get(conn, Routes.shared_with_you_index_path(conn, :index))
+      assert html_response(conn, 200) =~ "Shared Plots"
+    end
   end
 
   describe "Show" do
     setup [:create_shared_with_you]
+
+    test "renders the show page", %{conn: conn} do
+      conn = get(conn, Routes.shared_with_you_index_path(conn, :index))
+      assert html_response(conn, 200) =~ "Shared Plots"
+    end
   end
 end
--- test/plots_creator_web/live/your_plots_live_test.exs
+++ test/plots_creator_web/live/your_plots_live_test.exs
@@ -3,6 +3,8 @@ defmodule PlotsCreatorWeb.YourPlotsLiveTest do
 
   import PlotsCreator.DashboardFixtures
 
+  setup :register_and_log_in_user
+
   defp create_your_plots(_) do
     your_plots = your_plots_fixture()
     %{your_plots: your_plots}
@@ -10,9 +12,19 @@ defmodule PlotsCreatorWeb.YourPlotsLiveTest do
 
   describe "Index" do
     setup [:create_your_plots]
+
+    test "renders the index page", %{conn: conn} do
+      conn = get(conn, Routes.your_plots_index_path(conn, :index))
+      assert html_response(conn, 200) =~ "Listing Your Plots"
+    end
   end
 
   describe "Show" do
     setup [:create_your_plots]
+
+    test "renders the show page", %{conn: conn} do
+      conn = get(conn, Routes.your_plots_index_path(conn, :index))
+      assert html_response(conn, 200) =~ "Listing Your Plots"
+    end
   end
 end
